506166701fa0e57b7e78d43946ffccc9e52aa2b4
Thomas Connor <connort1@msu.edu>
2014-12-05 11:24:33 -0500

    Adding more IRAF params to hard-code

--- iraf_parameters.py
+++ iraf_parameters.py
@@ -31,3 +31,62 @@ def set_response(response):
     response.niterat = 1
     response.grow = 0
     return
+
+def set_identify_calibration(identify):
+    identify.section = 'middle line'
+    identify.databas = 'database'
+    identify.coordli = 'linelist'
+    identify.nsum = 5
+    identify.match = 10
+    identify.maxfeat = 50
+    identify.zwidth = 100
+    identify.ftype = 'emission'
+    identify.fwidth = 15
+    identify.cradius = 5
+    identify.thresho = 10
+    identify.minsep = 2
+    identify.function = 'spline3'
+    identify.order = 3
+    identify.niterat = 0
+    identify.low_rej = 3
+    identify.high_re = 3
+    identify.grow = 0
+    return
+
+def set_reidentify_calibration(reidentify):
+    reidentify.interac = 'No'
+    reidentify.section = 'middle line'
+    reidentify.newaps = 'Yes'
+    reidentify.overrid = 'Yes'
+    reidentify.refit = 'Yes'
+    reidentify.trace = 'Yes'
+    reidentify.step = 50
+    reidentify.nsum = 5
+    reidentify.shift = 0
+    reidentify.search = 0
+    reidentify.nlost = 10
+    reidentify.cradius = 5
+    reidentify.thresho = 10
+    reidentify.addfeat = 'No'
+    reidentify.coordli = 'linelist'
+    reidentify.match = 10
+    reidentify.maxfeat = 50
+    reidentify.minsep = 2
+    reidentify.databas = 'database'
+    return
+
+def set_fitcoords_calibration(fitcoords):
+    fitcoords.interac = 'Yes'
+    fitcoords.combine = 'Yes'
+    fitcoords.databas = 'database'
+    fitcoords.functio = 'chebyshev'
+    fitcoords.xorder = 3
+    fitcoords.yorder = 3
+    return
+
+def set_calibrate(calibrate):
+    #noao>imred>specred>calibrate
+    calibrate.extinct = 'Yes'
+    calibrate.flux = 'Yes'
+    calibrate.extinction = 'ondstsds$ctioextinct.dat'
+    return
